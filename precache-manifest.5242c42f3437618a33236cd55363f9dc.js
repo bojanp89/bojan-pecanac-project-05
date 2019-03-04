be122e06ee59dfa6a722c2998eb3b900ab9b9532
Bojan Pećanac <bo@Bojans-MacBook-Air.local>
2019-03-04 12:26:33 -0500

    Updates

---
+++ precache-manifest.5242c42f3437618a33236cd55363f9dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "179c0c077d6e1e364216",
+    "url": "/bojan-pecanac-project-05/static/js/runtime~main.179c0c07.js"
+  },
+  {
+    "revision": "15c3b94b3436d3489098",
+    "url": "/bojan-pecanac-project-05/static/js/main.15c3b94b.chunk.js"
+  },
+  {
+    "revision": "2175b7e2b03ce29ab1bc",
+    "url": "/bojan-pecanac-project-05/static/js/2.2175b7e2.chunk.js"
+  },
+  {
+    "revision": "15c3b94b3436d3489098",
+    "url": "/bojan-pecanac-project-05/static/css/main.6ddb3404.chunk.css"
+  },
+  {
+    "revision": "3b65812c771d25d0760621624dcdf13d",
+    "url": "/bojan-pecanac-project-05/index.html"
+  }
+];
